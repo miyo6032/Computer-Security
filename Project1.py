a1b31c036ea7eba5365c188b7c1689a2773c252b
Michael <miyo6032@colorado.edu>
2020-01-29 13:02:17 -0700

    Solve length extension

--- Project1.py
+++ Project1.py
@@ -9,22 +9,20 @@ x = "Good advice"
 
 h = md5()
 h.update(eight) # Add the 8 byte code with padding
-h = md5(state=bytes.fromhex(h.hexdigest()), count=get_encoding_length(len(eight))) # Get the hexdigest and length
 h.update(m) # Add the actual message
 print("Original Hex:", h.hexdigest())
 
 # This should be the same as the previous hexdigest
 h2 = md5()
-h2.update(eight.encode() + padding(len(eight)*8) + m.encode())
+h2.update(eight.encode() + m.encode())
 print("Original Hex:", h2.hexdigest())
 
 # Guess the length of the encoded message
-bits = get_encoding_length(len(eight)) + get_encoding_length(len(m))
+bits = get_encoding_length(len(eight) + len(m))
 h3 = md5(state=bytes.fromhex(h.hexdigest()), count=bits) # Input the token from the message
 h3.update(x) # Add in the malicious message
 print("New Hex:", h3.hexdigest())
 
 # This should be the same as the previous hexdigest
-h2 = md5()
-h2.update(eight.encode() + padding(len(eight)*8) + m.encode() + padding(len(m)*8) + x.encode())
+h2 = md5(eight.encode() + m.encode() + padding((len(eight) + len(m)) * 8) + x.encode())
 print("New Hex2:", h2.hexdigest())
--- len_ext_attack.py
+++ len_ext_attack.py
@@ -1,34 +1,30 @@
 import sys
 from pymd5 import md5, padding
 import re
-from urllib.parse import quote, unquote
+from urllib.parse import quote
 
 def get_encoding_length(length):
 	return (length + len(padding(length * 8))) * 8
 
 if len(sys.argv) >= 2:
-	original_link = quote(sys.argv[1])
-	print(original_link)
-	result = re.search(r'command%3D.*', original_link) # Find the original message
-	if(result != None):
-		message = original_link[result.start():] # Convert to string
-		message_length = len(message)
-		passcode_length = 8
-		
-		bits = 64 + get_encoding_length(message_length)
-		h = md5(state=bytes.fromhex("7976a258772fe07d857fef32a19ed611"), count=bits)
-		malicious_message = quote("&command=UnlockSafes")
-		print("\n")
-		print("Original Hex:", h.hexdigest())
-		h.update(malicious_message)
-		new_token = h.hexdigest()
+	original_link = sys.argv[1]
 
-		print("\n")
-		print("New Token:", new_token)
-		print("\n")
-		print("Message:", message)
-		print("\n")
-		print("New Message", malicious_message)
-		print("\n")
-		# Substitute new token and the new message into the url
-		print(unquote(re.sub(r'api\?token=.*$', "api?token={}&{}{}".format(new_token, message, malicious_message), unquote(original_link))))
+	result = re.search(r'command=.*', original_link) # Find the original message
+	token_result = re.search(r'token=.*', original_link) # Find original token
+	if(result != None and token_result != None):
+		message = original_link[result.start():]
+		old_token = original_link[token_result.start() + 6:token_result.start() + 6 + 32]
+		message_length = len(message)
+		secret_length = 8 # Assumption that the secret is always 8 bytes
+		
+		bits = get_encoding_length(secret_length + message_length) # Calculate bit position of message and padding
+		h = md5(state=bytes.fromhex(old_token), count=bits)
+		malicious_message = "&command=UnlockSafes"
+		h.update(malicious_message)
+		new_token = h.hexdigest() # Get our new token, which is secret||message||pad_m||x||pad_x
+
+		# Format the link to match secret||message||pad_m||x
+		previous_padding = quote(padding((message_length + secret_length) * 8))
+		subbed_link = re.sub(r'api\?token=.*$', "api?token=" + new_token + "&" + message + previous_padding + malicious_message, original_link)
+
+		print(subbed_link)
