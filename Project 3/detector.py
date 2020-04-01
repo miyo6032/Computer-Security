ea91662c6104a7734414fb140f63830e35e75ae5
Michael Yoshimura <miyo6032@colorado.edu>
2020-03-31 21:06:14 -0600

    Start project 3 part 4

--- Project 3/detector.py
+++ Project 3/detector.py
@@ -1,8 +1,31 @@
 #!/usr/bin/python3
 
 import scapy
-from scapy.utils import rdpcap
+from scapy.all import *
+from collections import defaultdict
 
 if __name__ == '__main__':
-    a=rdpcap("proj3.pcap")
-    print(a)
+    packets = rdpcap("proj3.dump.anon")
+
+    # Final goal is to have a dictionary of ip addresses
+    # with the count of syn packets sent and the count of syn_ack packet
+    # received
+
+    ip_packets = defaultdict(lambda: {"syn": 0, "synack": 0})
+    
+    for packet in packets:
+        if(packet.haslayer(TCP)):
+            tcp = packet[TCP]
+            ip = packet[IP]
+            syn_value = 2
+            synack_value = 18
+            
+            if tcp.flags.value == syn_value:
+                ip_packets[ip.src]["syn"]+=1
+            
+            elif tcp.flags.value == synack_value:
+                ip_packets[ip.dst]["synack"]+=1
+
+    for ip, counts in ip_packets.items():
+        if counts["syn"] > 3 * counts["synack"]:
+            print(ip)
